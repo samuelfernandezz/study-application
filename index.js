51a7cac1b8bee3557f4c68759826ec094fdffadb
samuelfernandezz <115535391+samuelfernandezz@users.noreply.github.com>
2022-10-16 16:44:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let openpo = document.getElementById('open');
+let closepo = document.getElementById('close');
+
+function openpopup(){
+openpo.classList.add("active");
+}
+function closepopup(){
+    openpo.classList.remove("active");
+ }
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- CSS only -->
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="list.css">
+
+</head>
+<body>
+
+ <?php include('profile.php') ?>
+ <?php include('boxes.php') ?>
+ <?php include('list.php') ?>
+ <?php include('menunotf.php') ?>
+ <?php include('teachers.php') ?>
+ <?php include('conn.php') ?>
+
+ 
+ <script src="index.js"></script>
+</body>
+</html>
